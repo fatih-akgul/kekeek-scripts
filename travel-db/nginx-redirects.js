e82f886e3b54874b51e8eaee6ea589f93ece9a2f
Fatih Akgul <akguls@gmail.com>
2019-10-04 11:15:36 -0400

    Create script to print out nginx 301 permanent redirects

---
+++ travel-db/nginx-redirects.js
@@ -0,0 +1,45 @@
+require('dotenv').config();
+const mysql = require('mysql');
+
+const SQL_CATEGORIES = `SELECT fileName FROM categories WHERE fileName like '%\_%'`;
+const SQL_PAGES = `SELECT fileName FROM pages WHERE fileName like '%\_%'`;
+
+const CUSTOM_ARTICLE_MAPPINGS = {
+  "health": "healthcare",
+  "turkey_misconceptions": "misconceptions",
+  "money": "turkish-money"
+};
+
+const connection = mysql.createConnection({
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASS,
+  database: process.env.DB_DATABASE
+});
+
+const processCustom = () => {
+  for (const oldArticleIdentifier in CUSTOM_ARTICLE_MAPPINGS) {
+    console.log(`rewrite ^/travel_turkey_${oldArticleIdentifier}$ /${cleanIdentifier(CUSTOM_ARTICLE_MAPPINGS[oldArticleIdentifier])} permanent;`)
+  }
+};
+
+const processDatabase = (query, prefix) => {
+  connection.query(query, [], (error, results, fields) => {
+    if (error) {
+      return console.error(error.message);
+    }
+
+    for (const result of results) {
+      const identifier = result.fileName;
+      console.log(`rewrite ^/${prefix}${identifier}$ /${cleanIdentifier(identifier)} permanent;`)
+    }
+  });
+};
+
+const cleanIdentifier = (identifier) => {
+  return identifier.replace(/_/g, '-');
+};
+
+processCustom();
+processDatabase(SQL_CATEGORIES, 'travel_turkey_');
+processDatabase(SQL_PAGES, 'turkey_');
